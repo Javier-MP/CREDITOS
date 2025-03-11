453653b89f8b38c0e28669aca3aa06045dcd9a6c
Giovany Martínez <141355644+Javier-MP@users.noreply.github.com>
2025-03-11 00:10:57 -0600

    Add files via upload

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Permitir peticiones desde el frontend
+CORS(app)  
 
-creditos = []  # Lista para almacenar los créditos
+creditos = []  
 
 @app.route("/creditos", methods=["POST"])
 def registrar_credito():
@@ -12,12 +12,11 @@ def registrar_credito():
     if not data or "cliente" not in data or "monto" not in data:
         return jsonify({"error": "Datos inválidos"}), 400
 
-    creditos.append(data)  # Guarda el crédito en la lista
+    creditos.append(data)  
     return jsonify({"mensaje": "Crédito registrado", "data": data}), 201
 
 @app.route("/creditos", methods=["GET"])
 def obtener_creditos():
-    return jsonify(creditos)  # Devuelve la lista de créditos
-
+    return jsonify(creditos)  
 if __name__ == "__main__":
     app.run(debug=True)
